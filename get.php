0c094c680c34aed0b57a61db4742fa54f001c5e6
Sagar Pamnani <sagarpamnani77@gmail.com>
2018-04-04 19:10:10 +0530

    update in get.php

---
+++ get.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Get Data</title>
+    </head>
+
+    <body>
+        <form action="getUserStatus.php" method="get">
+            Enter Status: <input type="text" name="status"> <input type="submit" value="Get">
+        </form>
+    </body>
+<html>
