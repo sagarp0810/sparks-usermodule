c2b9a750a5fc8fa0238ec752f514f1c80210f5f3
Sagar Pamnani <sagarpamnani77@gmail.com>
2018-04-04 18:47:29 +0530

    update

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Sparks Usermodule</title>
+    </head>
+
+    <body>
+        <a href="get.php"><button>Get Data</button></a>
+        <a href="update.php"><button>Update Data</button></a>
+    </body>
+<html>
