3e49d85980b763b07e1e96cf3a1ad348e1914819
Sagar Pamnani <sagarpamnani77@gmail.com>
2018-04-11 19:37:26 +0530

    update docs

--- index.php
+++ index.php
@@ -1,55 +1,19 @@
-<?php
-    require_once('dbConnect.php'); //connect to database
-
-    $query = "select * from user";
-    $result = mysqli_query($conn,$query);
-?>
-
 <html>
-    <head>
-        <title>Sparks Usermodule</title>
-    </head>
+<body>
+	<h1>Documentation</h1><br><br>
+<a href="getAllUserPaginationDoc.html"> Pagination API</a><br><br>
 
-    <body>
-        <table>
-			<thead>
-				<tr>
-                    <th>S No.</th>
-    				<th>EmailId</th>
-    				<th>Status</th>
-                    <th>Hashtags</th>
-                    <th>Roles</th>
-                    <th>TechnologyRatings</th>
-                    <th>PeopleRatings</th>
-                    <th>is_deleted</th>
-                    <th>deletedDate</th>
-				</tr>
-			</thead>
-
-            <!--fetch and display data from MySQL-->
-            <?php
-                $i=1;
-
-                while($row = mysqli_fetch_array($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $i . "</td>";
-                    echo "<td>" . $row["EmailId"] . "</td>";
-                    echo "<td>" . $row["Status"] . "</td>";
-                    echo "<td>" . $row["Hashtags"] . "</td>";
-                    echo "<td>" . $row["Roles"] . "</td>";
-                    echo "<td>" . $row["TechnologyRatings"] . "</td>";
-                    echo "<td>" . $row["PeopleRatings"] . "</td>";
-                    echo "<td>" . $row["is_deleted"] . "</td>";
-                    echo "<td>" . $row["deletedDate"] . "</td>";
-                    echo "</tr>";
-                    ++$i;
-                }
-            ?>
-
-        </table>
-        <br><hr>
-
-        <a href="get.php"><button>Get Data</button></a>
-        <a href="update.php"><button>Update Data</button></a>
-    </body>
-<html>
+<a href="getUserPeopleRatingDoc.html"> Get User People Rating API</a><br><br>
+<a href="getUserTechRatingDoc.html"> Get User Tech Rating API</a><br><br>
+<a href="getUserStatusDoc.html"> Get User Status API</a><br><br>
+<a href="getUserRoleDoc.html"> Get User Role API</a><br><br>
+<a href="getUserDoc.html"> Get User Created After API</a><br><br>
+<a href="getUserHashTagsDoc.html"> Get User Hashtag API</a><br><br>
+<a href="updatePeopleRatingDoc.html"> Update People Rating API</a><br><br>
+<a href="updateTechRatingDoc.html"> Update Tech Rating API</a><br><br>
+<a href="bulkUpdateuserDoc.html"> Bulk Update User API</a><br><br>
+<a href="updateStatusDoc.html"> Update Status API</a><br><br>
+<a href="updateUserHashTagsDoc.html"> Update User Hashtags API</a><br><br>
+<a href="updateUserRoleDoc.html"> Update User role API</a><br><br>
+</body>
+</html>
